9ad0b20ad100b25045b6a62efe12789835878430
Paul Bottein <paul.bottein@gmail.com>
2015-08-26 10:38:25 +0200

    Add mongoose connection by URI

--- model/db.js
+++ model/db.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
-
 var config = require('config');
-var mongo = config.get('MongoDB');
 
-mongoose.connect('mongodb://' + mongo.user + ':' + mongo.password + '@' + mongo.url + ':' + mongo.port);
+mongoose.connect(config.get('MongoDB.uri'));
