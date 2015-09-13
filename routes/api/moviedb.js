8c2a9252c73de655cc30a1e77b10f735302ac875
Bottein Paul <paul.bottein@gmail.com>
2015-09-13 14:19:35 +0200

    Error management

--- routes/api/moviedb.js
+++ routes/api/moviedb.js
@@ -7,21 +7,29 @@ var mdb = MovieDB(config.get('MovieDB.api.key'));
 exports.searchMovie = function (req, res) {
     mdb.searchMovie({query: req.query.term, language: "en"}, function(error, response){
         res.contentType('json');
-        var movies = [];
-        for (var i in response.results) {
-            var result = response.results[i];
-            year =  result.release_date ? result.release_date.split("-")[0] : undefined;
-            var movie = {
-                id: result.id,
-                name: result.original_title,
-                image: result.poster_path,
-                year: year
-            };
-            movies.push(movie);
+        if (response) {
+            var movies = [];
+            for (var i in response.results) {
+                var result = response.results[i];
+                year =  result.release_date ? result.release_date.split("-")[0] : undefined;
+                var movie = {
+                    id: result.id,
+                    name: result.original_title,
+                    image: result.poster_path,
+                    year: year
+                };
+                movies.push(movie);
+            }
+            res.send(
+                {results: movies}
+            );
         }
-        res.send(
-            {results: movies}
-        );
+        else {
+            res.send(
+                {results: []}
+            );
+        }
+        
     });
 };
 
@@ -29,19 +37,25 @@ exports.searchTv = function (req, res) {
     console.log(req.query);
     mdb.searchTv({query: req.query.term, language: "en"}, function(error, response){
         res.contentType('json');
-        var tvs = [];
-        for (var i in response.results) {
-            var result = response.results[i];
-            var tv = {
-                id: result.id,
-                name: result.original_name,
-                image: result.poster_path
-            };
-            tvs.push(tv);
+        if (response) {
+            var tvs = [];
+            for (var i in response.results) {
+                var result = response.results[i];
+                var tv = {
+                    id: result.id,
+                    name: result.original_name,
+                    image: result.poster_path
+                };
+                tvs.push(tv);
+            }
+            res.send(
+                {results: tvs}
+            );
+        else {
+            res.send(
+                {results: []}
+            );
         }
-        res.send(
-            {results: tvs}
-        );
     });
 };
 
@@ -75,37 +89,49 @@ exports.getTvSeasonInfo = function (req, res) {
 exports.getSeasons = function (req, res) {
     mdb.tvInfo({id: req.query.id, language: "en"}, function(error, response) {
         res.contentType('json');
-        var seasons = [];
-        for (var i in response.seasons) {
-            var result = response.seasons[i];
-            var season = {
-                season_number: result.season_number,
-                episode_count: result.episode_count,
-                air_date: result.air_date
-            };
-            seasons.push(season);
+        if (response) {
+            var seasons = [];
+            for (var i in response.seasons) {
+                var result = response.seasons[i];
+                var season = {
+                    season_number: result.season_number,
+                    episode_count: result.episode_count,
+                    air_date: result.air_date
+                };
+                seasons.push(season);
+            }
+            res.send(
+                {seasons: seasons}
+            );
+        else {
+            res.send(
+                {seasons: []}
+            );
         }
-        res.send(
-            {seasons: seasons}
-        );
     });
 };
 
 exports.getEpisodes = function (req, res) {
     mdb.tvSeasonInfo({id: req.query.id, season_number: req.query.season_number, language: "en"}, function(error, response) {
     res.contentType('json');
-        var episodes = [];
-        for (var i in response.episodes) {
-            var result = response.episodes[i];
-            var episode = {
-                episode_number: result.episode_number,
-                air_date: result.air_date,
-                name: result.name
-            };
-            episodes.push(episode);
+        if (response) {
+            var episodes = [];
+            for (var i in response.episodes) {
+                var result = response.episodes[i];
+                var episode = {
+                    episode_number: result.episode_number,
+                    air_date: result.air_date,
+                    name: result.name
+                };
+                episodes.push(episode);
+            }
+            res.send(
+                {episodes: episodes}
+            );
+        else {
+            res.send(
+                {episodes: []}
+            );
         }
-        res.send(
-            {episodes: episodes}
-        );
     });
 };
